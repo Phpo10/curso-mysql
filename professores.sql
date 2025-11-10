1f04652c26299e4bd49841d831302298228ac356
João Vítor Almeida <128109854+almeidaprog@users.noreply.github.com>
2025-11-10 20:19:22 -0300

    updated professores

--- professores.sql
+++ professores.sql
@@ -1,9 +1,3 @@
-create table professores (
-	login int primary key auto_increment,
-	nome varchar(50) not null,
-	especialidade varchar(50)
-);
-
 insert into professores (nome, especialidade) values
 	('Carlos Silva', 'Java'),
 	('Maria Santos', 'Banco de Dados'),
@@ -13,21 +7,12 @@ insert into professores (nome, especialidade) values
 
 select * from professores;
 
-create table alunos_professores (
-    aluno_id int,
-    professor_id int,
-    foreign key (aluno_id) references alunos(matricula),
-    foreign key (professor_id) references professores(login)
-);
-
-
 insert into alunos_professores (aluno_id, professor_id) values
 (2023501, 1),
 (2023502, 2),
 (2023503, 3),
 (2023504, 4),
 (2023505, 5);
-
 
 select a.nome as aluno, p.nome as professor
 from alunos_professores ap
